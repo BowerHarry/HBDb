f5a225d2df697e2d525295224207f86661c97b1e
harry <harryisambard@gmail.com>
2024-10-04 16:23:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true }, // Not needed for Vite 5+
+  server: { https: true, 'Content-Type': contentType, 'Access-Control-Allow-Origin': '*'  }, // Not needed for Vite 5+
   plugins: [react(), mkcert() ],
   build: {
     target: "ES2022" 
